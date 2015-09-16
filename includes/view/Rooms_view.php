5027c03c83e6450c733326c38186754a09942f55
ElectricMaxxx <maximilian.berghoff@gmx.de>
2015-09-16 21:10:55 +0200

    first style changes

--- includes/view/Rooms_view.php
+++ includes/view/Rooms_view.php
@@ -1,10 +1,12 @@
 <?php
 
-function Room_name_render($room) {
-  global $privileges;
-  if (in_array('admin_rooms', $privileges))
-    return '<a href="' . room_link($room) . '">' . glyph('map-marker') . $room['Name'] . '</a>';
-  return glyph('map-marker') . $room['Name'];
-}
+function Room_name_render($room)
+{
+    global $privileges;
 
-?>
+    if (in_array('admin_rooms', $privileges)) {
+        return sprintf('<a href="%s">%s</a>', room_link($room), glyph('map-marker') . $room['Name']);
+    }
+
+    return glyph('map-marker') . $room['Name'];
+}
